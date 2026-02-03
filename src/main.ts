aa058826b023069e901af418e54daff4c0c05c63
Firegame224 <xg474627@gmail.com>
2026-02-03 23:26:15 +0700

    chore: setup cors & change app port

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["*"],
+  })
   app.useGlobalPipes(new ValidationPipe({
     forbidUnknownValues: true,
     whitelist: true,
     transform: true,
     forbidNonWhitelisted: true
   }))
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
