78d4caee91a1d010441e116cf629884f5bc7d693
Firegame224 <xg474627@gmail.com>
2026-02-05 00:08:55 +0700

    feat(user): add user module, controller, service, and repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { PrismaController } from './modules/prisma/prisma.controller';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   controllers: [AppController, PrismaController],
   providers: [AppService],
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env", }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env", }), PrismaModule, UserModule],
 })
 export class AppModule { }
