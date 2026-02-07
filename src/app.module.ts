4147706ef417938ab55b6e1aebea08dc271f6829
Firegame224 <xg474627@gmail.com>
2026-02-07 23:41:36 +0700

    feat(minio): add MinIO service configuration to docker-compose refactor(prisma): export PrismaService in PrismaModule refactor(user): export UserRepository in UserModule refactor(app): remove unused PrismaController from AppModule chore(gitignore): add minio-data to .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
-import { PrismaController } from './modules/prisma/prisma.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  controllers: [AppController, PrismaController],
+  controllers: [AppController],
   providers: [AppService],
   imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env", }), PrismaModule, UserModule, AuthModule],
 })
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -7,6 +7,7 @@ import { UserRepository } from './user.repository';
 @Module({
   controllers: [UserController],
   providers: [UserService, UserRepository],
-  imports: [PrismaModule]
+  imports: [PrismaModule],
+  exports: [UserRepository]
 })
 export class UserModule { }
